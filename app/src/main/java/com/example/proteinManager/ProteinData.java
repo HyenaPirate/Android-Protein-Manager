9bf6a9a882c4eabaa40fb998cf7ba8d2f3716948
Tomasz201PL <117272459+Tomasz201PL@users.noreply.github.com>
2025-04-15 16:07:26 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/proteinManager/ProteinData.java
@@ -0,0 +1,11 @@
+package com.example.proteinManager;
+
+public class ProteinData {
+    public int targetProtein;
+    public int consumedProtein;
+
+    public ProteinData(int targetProtein, int consumedProtein) {
+        this.targetProtein = targetProtein;
+        this.consumedProtein = consumedProtein;
+    }
+}
